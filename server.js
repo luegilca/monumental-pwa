b258f831c1d0ec1b7e2d00f7c3cfbafbd6144db3
Luis Ernesto Gil Castellanos <luegilca@unal.edu.co>
2019-03-04 13:51:24 -0500

    Fixed port

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
-app.listen(port);
+app.listen(5000);
 console.log('server started '+ port);
